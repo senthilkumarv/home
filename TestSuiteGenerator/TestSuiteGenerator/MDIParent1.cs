0624d376fe282dd17f1bb86a94df07a44131e585
senthilkumarv <senthil.vijay.kumar@gmail.com>
2010-04-27 14:00:49 +0530

    MDI Menus Altered

--- TestSuiteGenerator/TestSuiteGenerator/MDIParent1.cs
+++ TestSuiteGenerator/TestSuiteGenerator/MDIParent1.cs
@@ -18,92 +18,6 @@ namespace TestSuiteGenerator
             InitializeComponent();
         }
         
-
-        private void ShowNewForm(object sender, EventArgs e)
-        {
-            Form childForm = new Form();
-            childForm.MdiParent = this;
-            childForm.Text = "Window " + childFormNumber++;
-            childForm.Show();
-        }
-
-        private void OpenFile(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
-            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
-            {
-                string FileName = openFileDialog.FileName;
-            }
-        }
-
-        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
-            if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
-            {
-                string FileName = saveFileDialog.FileName;
-            }
-        }
-
-        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            toolStrip.Visible = toolBarToolStripMenuItem.Checked;
-        }
-
-        private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            statusStrip.Visible = statusBarToolStripMenuItem.Checked;
-        }
-
-        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.Cascade);
-        }
-
-        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.TileVertical);
-        }
-
-        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.TileHorizontal);
-        }
-
-        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.ArrangeIcons);
-        }
-
-        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            foreach (Form childForm in MdiChildren)
-            {
-                childForm.Close();
-            }
-        }
-
         private void MDIParent1_Load(object sender, EventArgs e)
         {
             testSuite.Nodes[0].Expand();
@@ -263,11 +177,39 @@ namespace TestSuiteGenerator
             plsql.Show();
         }
 
-        private void testSuite_AfterSelect(object sender, TreeViewEventArgs e)
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
-
+            addTableToolStripMenuItem_Click(sender, e);
         }
 
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            addStaticCheckToolStripMenuItem_Click(sender, e);
+        }
 
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            addSetupToolStripMenuItem_Click(sender, e);
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            addCleanupQueryToolStripMenuItem_Click(sender, e);
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            addMessageToolStripMenuItem_Click(sender, e);
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            addQueryTestCaseToolStripMenuItem_Click(sender, e);
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            addPLSQLTestCaseToolStripMenuItem_Click(sender, e);
+        }
     }
 }
